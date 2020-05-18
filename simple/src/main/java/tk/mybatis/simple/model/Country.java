ec73e4b977a3adbad2ab99ef018ed42f6f590814
jamesKen <37204594+jamesKen@users.noreply.github.com>
2020-05-18 10:51:31 +0800

    Add files via upload

---
+++ simple/src/main/java/tk/mybatis/simple/model/Country.java
@@ -0,0 +1,38 @@
+package tk.mybatis.simple.model;
+
+/**
+ * @ClassName Country
+ * @Description TODO
+ * @Author zhangxidong
+ * @Date 2020/5/18
+ * @Version 1.0
+ */
+public class Country {
+    private Long id;
+    private String countryname;
+    private String countrycode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+}
---
+++ simple/src/test/java/tk/mybatis/simple/mapper/CountryMapperTest.java
@@ -0,0 +1,54 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tk.mybatis.simple.model.Country;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+/**
+ * @ClassName CountryMapperTest
+ * @Description TODO
+ * @Author zhangxidong
+ * @Date 2020/5/18
+ * @Version 1.0
+ */
+public class CountryMapperTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        try{
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        }catch (IOException ignore){
+            ignore.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            List<Country> countryList = sqlSession.selectList("selectAll");
+            printCountryList(countryList);
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    private void printCountryList(List<Country> countryList) {
+        for (Country country : countryList) {
+            System.out.printf("%-4d%4s%4s\n",
+                    country.getId(), country.getCountryname(), country.getCountrycode());
+        }
+    }
+}
